47a0f494217871e1e4ee1f6da7429d0293049cf6
alvinly <alvinly74@gmail.com>
2015-11-07 17:06:29 -0800

    initial commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+
+  def index
+    puts "got 'em"
+    render json: "yooo"
+  end
+
+  def create
+    puts "got 'em"
+    render json: "yooo"
+  end
+
+end
