45015b66f4918a13e022020d6b9d904a005dbfae
alvinly <alvinly74@gmail.com>
2015-11-07 18:54:48 -0800

    update home create method

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
+require 'net/https'
+
 class HomesController < ApplicationController
 
   def index
@@ -6,8 +8,16 @@ class HomesController < ApplicationController
   end
 
   def create
-    puts "got 'em"
-    render json: "yooo"
+    # puts "got 'em"
+    # render json: "yooo"
+    webhook = "https://hooks.slack.com/services/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"
+
+    req = Net::HTTP::Post.new('webhook')
+    req.set_form_data(payload={"location" => 'hrere', "time" => 'now'})
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    http.get('/')
   end
 
 end
